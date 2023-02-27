2397618c51263b0bb75084407c21387d483f1053
Akshat <akshat26april@gmail.com>
2023-02-27 17:02:46 +0530

    Developed Realtime Location Fetching and Respectively Temperature Fetching

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { ImageBackground, StyleSheet, Text, View, Image } from "react-native";
 import axios from "axios";
+import Geolocation from "@react-native-community/geolocation";
 
 export default function App() {
+  // Day, Date, Month Fetch Function Starts
+
   const [currentDay, setCurrentDay] = useState("");
   const [currentDate, setCurrentDate] = useState("");
 
@@ -18,6 +21,60 @@ export default function App() {
     return () => clearInterval(interval);
   }, []);
 
+  // Day, Date, Month Fetch Function Ends
+
+  // API INFO Starts
+
+  const API_KEY = "fde70b7b510d418c6126c7433ab077c4";
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric&q=`;
+  
+  // API INFO Ends
+
+  // Location Fetching Starts
+
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+        axios
+          .get(URL)
+          .then((response) => {
+            setWeatherData(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+      (error) => {
+        console.log(error);
+      },
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }, []);
+
+  // Location Fetching Ends
+
+  // Temperature Fetch By Location Function Starts
+
+  const [temperature, setTemperature] = useState(null);
+  const [locationName, setLocationName] = useState(null);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { latitude, longitude } = position.coords;
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setTemperature(data.main.temp);
+    });
+  }, []);
+
+  // Temperature Fetch By Location Function Ends
+
+  // MAIN RENDER FUNCTION STARTS
+
   return (
     <ImageBackground
       style={styles.backgroundImage}
@@ -30,8 +87,14 @@ export default function App() {
           </Text>
         </Text>
       </View>
-      <View style={styles.CurrentLocation}>
-        <Text style={styles.CurrentLocationText}>Sundarnagar</Text>
+      <View>
+        {weatherData ? (
+          <View style={styles.CurrentLocation}>
+            <Text style={styles.CurrentLocationText}>{weatherData.name}</Text>
+          </View>
+        ) : (
+          <Text>Loading...</Text>
+        )}
       </View>
       <View style={styles.weatherImage}>
         <Image
@@ -41,7 +104,9 @@ export default function App() {
         <Text style={styles.weatherImageText}>Partly Cloudy</Text>
       </View>
       <View style={styles.temp}>
-        <Text style={styles.tempText}>27°C</Text>
+        <Text style={styles.tempText}>
+          {temperature ? `${temperature} °C` : "Loading..."}
+        </Text>
       </View>
     </ImageBackground>
   );
@@ -97,7 +162,7 @@ const styles = StyleSheet.create({
     marginTop: "1rem",
   },
   tempText: {
-    fontSize: "5rem",
+    fontSize: "4rem",
     fontFamily: "Montserrat",
     color: "#f1f1f1",
     fontWeight: "100",
