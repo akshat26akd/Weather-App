f3d4c5284f8f1fb0078826191b99172b9d3ca49d
Akshat <akshat26april@gmail.com>
2023-02-27 15:07:13 +0530

    Created Realtime Day, Date & Month and added weather type image and temperature.

--- App.js
+++ App.js
@@ -1,21 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { ImageBackground, StyleSheet, Text, View, Image } from "react-native";
+import axios from "axios";
 
 export default function App() {
+  const [currentDay, setCurrentDay] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const dateObj = new Date();
+      setCurrentDay(dateObj.toLocaleDateString("en-US", { weekday: "long" }));
+      const month = dateObj.toLocaleString("default", { month: "long" });
+      const day = dateObj.getDate();
+      setCurrentDate(`${day} ${month}`);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      style={styles.backgroundImage}
+      source={require("./assets/BG_Gradient.png")}
+    >
+      <View style={styles.Datecontainer}>
+        <Text>
+          <Text style={styles.DateText}>
+            {currentDay}, {currentDate}
+          </Text>
+        </Text>
+      </View>
+      <View style={styles.CurrentLocation}>
+        <Text style={styles.CurrentLocationText}>Sundarnagar</Text>
+      </View>
+      <View style={styles.weatherImage}>
+        <Image
+          source={require("./assets/PartlyCloudyDay.png")}
+          style={{ width: 150, height: 150 }}
+        />
+        <Text style={styles.weatherImageText}>Partly Cloudy</Text>
+      </View>
+      <View style={styles.temp}>
+        <Text style={styles.tempText}>27°C</Text>
+      </View>
+    </ImageBackground>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  Datecontainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: "7rem",
+  },
+
+  DateText: {
+    fontFamily: "Montserrat",
+    fontSize: "1.75rem",
+    color: "#f1f1f1",
+    fontStyle: "italic",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  CurrentLocation: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: "1rem",
+  },
+
+  CurrentLocationText: {
+    fontFamily: "Montserrat",
+    fontSize: "1.5rem",
+    fontVariant: ["small-caps"],
+    color: "#f1f1f1",
+    fontWeight: "500",
+    textAlign: "center",
+  },
+
+  weatherImage: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: "4rem",
+  },
+  weatherImageText: {
+    fontSize: "1.5rem",
+    fontFamily: "Montserrat",
+    color: "#f1f1f1",
+    fontWeight: "500",
+    textAlign: "center",
+  },
+  temp: {
+    marginTop: "1rem",
+  },
+  tempText: {
+    fontSize: "5rem",
+    fontFamily: "Montserrat",
+    color: "#f1f1f1",
+    fontWeight: "100",
+    textAlign: "center",
   },
 });
